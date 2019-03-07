cd07498955b324a49ce9aa8efb3c7acf38941143
adrestios <778158202@qq.com>
2019-03-07 14:42:20 +0800

    简单实现了一个Promise

--- myPromise.js
+++ myPromise.js
@@ -7,10 +7,12 @@ class myPromise {
     this._value = undefined
     this._onFulfilledCallback = []
     this._onRejectedCallback = []
+    this.resolve = this.resolve.bind(this)
+    this.reject = this.reject.bind(this)
     try {
       executor(this.resolve, this.reject)
     } catch(e) {
-      reject(e)
+      this.reject(e)
     }
   }
   resolve(value) {
@@ -18,16 +20,74 @@ class myPromise {
       this._status = 'fulfilled'
       this._value = value
     }
+    for(var i = 0; i < this._onFulfilledCallback.length; i++) {
+      this._onFulfilledCallback[i](value)
+    }
   }
   reject(err) {
     if (this._status === 'pending') {
       this._status = 'rejected'
       this._value = err
     }
+    for (var i = 0; i < this._onRejectedCallback.length; i++) {
+      this._onRejectedCallback[i](reason)
+    }
   }
   then(onResolved, onRejected) {
-    if (this._status === 'pending') {
-      this._onFulfilledCallback.push(onResolved)
+    onResolved = typeof onResolved === 'function' ? onResolved : v => v
+    onRejected = typeof onRejected === 'function' ? onRejected : r => {throw r}
+    if (this._status === 'fulfilled') {
+      return new myPromise((resolve, reject) => {
+        try {
+          var pc = onResolved(this._value)
+          if (pc instanceof myPromise) {
+            pc.then(resolve, reject)
+          }
+          resolve(pc)
+          console.log(resolve);
+        } catch(e) {
+          reject(e)
+        }
+      })
     }
+    if (this._status === 'rejected') {
+      return new myPromise((resolve, reject) => {
+        try {
+          var pc = onRejected(this._value)
+          if (pc instanceof myPromise) {
+            pc.then(resolve, reject)
+          }
+        } catch(e) {
+          reject(e)
+        }
+      })
+    }
+    if (this._status === 'pending') {
+      return new myPromise((resolve, reject) => {
+        this._onFulfilledCallback.push((value) => {
+          try {
+            var pv = onResolved(this._value)
+            if (pv instanceof myPromise) {
+              pv.then(resolve, reject)
+            }
+          } catch(e) {
+            reject(e)
+          }
+        })
+        this._onRejectedCallback.push((err) => {
+          try {
+            var pv = onRejected(this)
+            if (pv instanceof myPromise) {
+              pv.then(resolve, reject)
+            }
+          } catch(e) {
+            reject(e)
+          }
+        })
+      })
+    }
+  }
+  catch(onRejected) {
+    return this.then(undefined, onRejected)
   }
 }
